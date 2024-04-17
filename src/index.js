b5510afdab1c64652e945f864231c1ae823a4455
iamLusani <lusani.22mphigalale@gmail.com>
2024-04-17 18:30:37 +0200

    made it user friendly

--- src/index.js
+++ src/index.js
@@ -12,12 +12,16 @@ function createPoem(event) {
 
   let instructionElement = document.querySelector("#userInstruction");
 
-  let prompt = `Generate a poem about and make it about flowers using ${instructionElement}  `;
+  let prompt = ` User instruction: Generate a poem about ${instructionElement.value}  `;
   let context =
-    " You are romantic poet that write poems about differnt kind of flowers using descriptive words like love and romance  that are 6 lines short generate it in basic  HTML formate Do not include heading and ```html and do not put poem inside a text-box or drop-down box . May you please make it into a fluent paragraph.I would kindly like to ask you to be very descriptive and use one flower at a time. Dont forget to follow user-instruction.  Thank you";
+    " You are romantic poet that write poems about differnt kind of flowers using descriptive words like love and romance  that are 6 lines short do not display more. Generate it in basic  HTML formate Do not include heading and ```html and do not put poem inside a text-box or drop-down box . May you please make it into a fluent paragraph.I would kindly like to ask you to be very descriptive and use one flower at a time . Roses are not the only flowers . Please behave and follow all the instruction.Do not display HTML inside the poem . Thank you";
   let apiKey = "56ftc44o535ed4b849450cf23fa64b07";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showLove);
+
+  let poemElement = document.querySelector("#poem-box");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a ${instructionElement.value} poem for you.....`;
 }
 
 let formElement = document.querySelector("#poem-input-place");
